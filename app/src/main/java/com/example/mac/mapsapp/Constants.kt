aa59ba4f2817f3ad0ff02b3d447e72580d2e3167
LeoValentim <leo.valent@hotmail.com>
2017-09-11 08:43:19 -0300

    Api keys

--- app/src/main/java/com/example/mac/mapsapp/Constants.kt
+++ app/src/main/java/com/example/mac/mapsapp/Constants.kt
@@ -5,7 +5,7 @@ package com.example.mac.mapsapp
  */
 class Constants {
     companion object {
-        fun uberApiKey() : String = "" // UBER API KEY
-        fun googleApiKey() : String = "" // GOOGLE MAPS API KEY
+        fun uberApiKey() : String = "XzBnL_B_T9kqAb08JEutW5U196sH8rpMRtA1e2Kd"
+        fun googleApiKey() : String = "AIzaSyDEQU2yc4VKolHpMtRc4AK1RwyRR4yd4UE"
     }
 }
